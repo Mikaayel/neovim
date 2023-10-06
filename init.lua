8bed7ed31ecaa657a68a2f15212dedcf98c3a632
Mika <Mikaayel@users.noreply.github.com>
2023-10-06 20:14:40 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,20 @@
+-- spacing
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+
+-- gui
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.guifont = "monospace:h17"
+vim.opt.wrap = false
+
+-- undo
+vim.opt.undofile = true
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- mouse
+vim.opt.mouse = "a"
